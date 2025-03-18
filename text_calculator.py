4fbd3d79b6ec1f7e0deb42ea18e98880dbe761ae
TORVALD <ivser11@tut.by>
2025-03-18 21:14:03 +0300

    добавил info
    Signed-off-by: TORVALD <ivser11@tut.by>

---
+++ text_calculator.py
@@ -0,0 +1,10 @@
+from calculator import Calculator
+
+# Т.З.
+# 1.Разраб.1 реализовать функцию умножения.
+# 1.Разраб.2 реализовать функцию деления.
+
+
+calc = Calculator()
+calc.sum(45, 5)
+calc.print_last_res()
